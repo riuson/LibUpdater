88edbfee20fe172eb0b2b835f2333e93e4110dc0
Vladimir <riuson@gmail.com>
2023-02-07 22:50:07 +0500

    Test for files discovered

--- LibUpdater.Tests/Utils/DirectoryScannerTests.cs
+++ LibUpdater.Tests/Utils/DirectoryScannerTests.cs
@@ -6,7 +6,24 @@ namespace LibUpdater.Tests.Utils
     internal class DirectoryScannerTests
     {
         [Test]
-        public void DirectoryScannerShouldScanTree()
+        public void DirectoryScannerShouldScan()
+        {
+            var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var samplesPath = Path.Combine(assemblyPath, "Samples2", "DirStruct");
+
+            var scanner = new DirectoryScanner();
+
+            var items = scanner.ScanTree(samplesPath);
+
+            Assert.That(items.Count(), Is.EqualTo(4));
+            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\file.txt")), Is.True);
+            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub1\file1.txt")), Is.True);
+            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\file2.txt")), Is.True);
+            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\sub3\file3.txt")), Is.True);
+        }
+
+        [Test]
+        public void DirectoryScannerShouldSort()
         {
             var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var samplesPath = Path.Combine(assemblyPath, "Samples2", "DirStruct");
