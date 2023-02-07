157058e09a08aaaf185acd04c52d228d10da87d4
Vladimir <riuson@gmail.com>
2023-02-07 22:35:56 +0500

    Add first test for scanner

---
+++ LibUpdater.Tests/Utils/DirectoryScannerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace LibUpdater.Tests.Utils
+{
+    internal class DirectoryScannerTests
+    {
+        [Test]
+        public void DirectoryScannerShouldScanTree()
+        {
+            var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var samplesPath = Path.Combine(assemblyPath, "Samples2", "DirStruct");
+
+            var scanner = new DirectoryScanner();
+
+            var items = scanner.ScanTree(samplesPath);
+
+            var sortedItems = items.OrderBy(x => x.Path);
+
+            Assert.That(items, Is.EqualTo(sortedItems));
+        }
+    }
+}
