ae48e708883d3154b18f0ad171e8a5054d553206
Vladimir <riuson@gmail.com>
2023-02-08 22:25:33 +0500

    Add tests for simple non-equality

--- LibUpdater/Analysis/TreeAnalyzer.cs
+++ LibUpdater/Analysis/TreeAnalyzer.cs
@@ -24,6 +24,9 @@ public class TreeAnalyzer
             };
         }
 
-        throw new NotImplementedException();
+        return new AnalyzeResult()
+        {
+            IsEquals = false,
+        };
     }
 }
