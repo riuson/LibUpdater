89a4469382008aa2996dfbc42181030cbd57fd33
Vladimir <riuson@gmail.com>
2023-02-10 20:23:52 +0500

    Extract Downloader to new file

---
+++ LibUpdater/Utils/Downloader.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace LibUpdater.Utils;
+
+internal class Downloader : IDownloader
+{
+    public string DownloadString(string uri)
+    {
+        using var client = new WebClient();
+        return client.DownloadString(uri);
+    }
+
+    public void DownloadFile(string uri, string path)
+    {
+        using var client = new WebClient();
+        client.DownloadFile(uri, path);
+    }
+}
--- LibUpdater/Utils/IDownloader.cs
+++ LibUpdater/Utils/IDownloader.cs
@@ -1,24 +1,7 @@
-﻿using System.Net;
-
-namespace LibUpdater.Utils;
+﻿namespace LibUpdater.Utils;
 
 public interface IDownloader
 {
     string DownloadString(string uri);
     void DownloadFile(string uri, string path);
-}
-
-internal class Downloader : IDownloader
-{
-    public string DownloadString(string uri)
-    {
-        using var client = new WebClient();
-        return client.DownloadString(uri);
-    }
-
-    public void DownloadFile(string uri, string path)
-    {
-        using var client = new WebClient();
-        client.DownloadFile(uri, path);
-    }
 }
