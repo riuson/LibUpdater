ce8a7f1ce480f3425393eb9f74366c1517256680
Vladimir <riuson@gmail.com>
2023-02-10 20:24:16 +0500

    Fix default version file name

--- LibUpdater/Utils/UpdateOptions.cs
+++ LibUpdater/Utils/UpdateOptions.cs
@@ -5,7 +5,7 @@ public class UpdateOptions
     public string TargetDir { get; set; }
     public string TempDir { get; set; }
     public string UpdatesUri { get; set; }
-    public string VersionFile { get; set; } = "version.json";
+    public string VersionFile { get; set; } = "version.txt";
     public string IndexFile { get; set; } = "index.json";
     public int DegreeOfParallelism { get; set; } = 1;
 }
