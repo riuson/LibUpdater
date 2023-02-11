8275b31cdfc9fb4bbb908f9104407c72d8319cae
Vladimir <riuson@gmail.com>
2023-02-11 19:15:48 +0500

    Add calculated data to result of analysis

--- LibUpdater/Analysis/AnalyzeResult.cs
+++ LibUpdater/Analysis/AnalyzeResult.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using LibUpdater.Data;
 
 namespace LibUpdater.Tests.Utils;
@@ -8,4 +9,10 @@ internal class AnalyzeResult : IAnalyzeResult
     public bool IsEquals { get; set; } = false;
     public IEnumerable<IFileItem> Obsolete { get; set; } = new IFileItem[] { };
     public IEnumerable<IArchiveItem> Added { get; set; } = new IArchiveItem[] { };
+    public int FilesToAddCount => Added.Count();
+    public int FilesToRemoveCount => Obsolete.Count();
+    public long BytesToRemove => Obsolete.Sum(x => x.Size);
+    public long BytesToAdd => Added.Sum(x => x.Size);
+    public long BytesToDownload => Added.Sum(x => x.ArchiveSize);
+    public long BytesChanged => BytesToAdd - BytesToRemove;
 }
--- LibUpdater/Analysis/IAnalyzeResult.cs
+++ LibUpdater/Analysis/IAnalyzeResult.cs
@@ -8,4 +8,11 @@ public interface IAnalyzeResult
     bool IsEquals { get; }
     IEnumerable<IFileItem> Obsolete { get; }
     IEnumerable<IArchiveItem> Added { get; }
+
+    int FilesToAddCount { get; }
+    int FilesToRemoveCount { get; }
+    long BytesToRemove { get; }
+    long BytesToAdd { get; }
+    long BytesToDownload { get; }
+    long BytesChanged { get; }
 }
