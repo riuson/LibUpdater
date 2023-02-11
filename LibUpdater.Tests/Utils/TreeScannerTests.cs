a1809a5a98c380bed0948bbf68c8969510d38277
Vladimir <riuson@gmail.com>
2023-02-11 09:37:21 +0500

    Add async methods to tree scanner

--- LibUpdater.Tests/Utils/TreeScannerTests.cs
+++ LibUpdater.Tests/Utils/TreeScannerTests.cs
@@ -1,64 +1,98 @@
 ﻿using System.Reflection;
 using LibUpdater.Utils;
 
-namespace LibUpdater.Tests.Utils
+namespace LibUpdater.Tests.Utils;
+
+internal class TreeScannerTests
 {
-    internal class TreeScannerTests
+    private string _samplesPath;
+
+    [SetUp]
+    public void Setup()
     {
-        private string _samplesPath;
+        var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        _samplesPath = Path.Combine(assemblyPath, "Samples2", "DirStruct");
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            this._samplesPath = Path.Combine(assemblyPath, "Samples2", "DirStruct");
-        }
+    [Test]
+    public void DirectoryScannerShouldScan()
+    {
+        var scanner = new TreeScanner();
 
-        [Test]
-        public void DirectoryScannerShouldScan()
-        {
-            var scanner = new TreeScanner();
+        var items = scanner.ScanTree(_samplesPath);
 
-            var items = scanner.ScanTree(this._samplesPath);
+        Assert.That(items.Count(), Is.EqualTo(4));
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\file.txt")), Is.True);
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub1\file1.txt")), Is.True);
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\file2.txt")), Is.True);
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\sub3\file3.txt")), Is.True);
+    }
 
-            Assert.That(items.Count(), Is.EqualTo(4));
-            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\file.txt")), Is.True);
-            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub1\file1.txt")), Is.True);
-            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\file2.txt")), Is.True);
-            Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\sub3\file3.txt")), Is.True);
-        }
+    [Test]
+    public async Task DirectoryScannerShouldScanAsync()
+    {
+        var scanner = new TreeScanner();
 
-        [Test]
-        public void DirectoryScannerShouldSort()
-        {
-            var scanner = new TreeScanner();
+        var items = await scanner.ScanTreeAsync(_samplesPath);
 
-            var items = scanner.ScanTree(this._samplesPath).Select(x => x.Path);
+        Assert.That(items.Count(), Is.EqualTo(4));
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\file.txt")), Is.True);
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub1\file1.txt")), Is.True);
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\file2.txt")), Is.True);
+        Assert.That(items.Any(x => x.Path.Contains(@"DirStruct\sub2\sub3\file3.txt")), Is.True);
+    }
 
-            var sortedItems = items.OrderBy(x => x);
+    [Test]
+    public void DirectoryScannerShouldSort()
+    {
+        var scanner = new TreeScanner();
 
-            Assert.That(items, Is.EqualTo(sortedItems));
-        }
+        var items = scanner.ScanTree(_samplesPath).Select(x => x.Path);
 
-        [Test]
-        public void DirectoryScannerShouldGetFileSize()
-        {
-            var scanner = new TreeScanner();
+        var sortedItems = items.OrderBy(x => x);
 
-            var items = scanner.ScanTree(this._samplesPath);
+        Assert.That(items, Is.EqualTo(sortedItems));
+    }
 
-            Assert.That(items.All(x => x.Size == 1), Is.True);
-        }
+    [Test]
+    public void DirectoryScannerShouldGetFileSize()
+    {
+        var scanner = new TreeScanner();
 
-        [Test]
-        public void DirectoryScannerShouldGetFileHash()
-        {
-            var scanner = new TreeScanner();
-            var expectedHash = "356a192b7913b04c54574d18c28d46e6395428ab"; // File with content "1".
+        var items = scanner.ScanTree(_samplesPath);
 
-            var items = scanner.ScanTree(this._samplesPath);
+        Assert.That(items.All(x => x.Size == 1), Is.True);
+    }
 
-            Assert.That(items.All(x => x.Hash == expectedHash), Is.True);
-        }
+    [Test]
+    public async Task DirectoryScannerShouldGetFileSizeAsync()
+    {
+        var scanner = new TreeScanner();
+
+        var items = await scanner.ScanTreeAsync(_samplesPath);
+
+        Assert.That(items.All(x => x.Size == 1), Is.True);
+    }
+
+    [Test]
+    public void DirectoryScannerShouldGetFileHash()
+    {
+        var scanner = new TreeScanner();
+        var expectedHash = "356a192b7913b04c54574d18c28d46e6395428ab"; // File with content "1".
+
+        var items = scanner.ScanTree(_samplesPath);
+
+        Assert.That(items.All(x => x.Hash == expectedHash), Is.True);
+    }
+
+    [Test]
+    public async Task DirectoryScannerShouldGetFileHashAsync()
+    {
+        var scanner = new TreeScanner();
+        var expectedHash = "356a192b7913b04c54574d18c28d46e6395428ab"; // File with content "1".
+
+        var items = await scanner.ScanTreeAsync(_samplesPath);
+
+        Assert.That(items.All(x => x.Hash == expectedHash), Is.True);
     }
 }
--- LibUpdater/Utils/TreeScanner.cs
+++ LibUpdater/Utils/TreeScanner.cs
@@ -1,37 +1,53 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using LibUpdater.Data;
 
-namespace LibUpdater.Utils
+namespace LibUpdater.Utils;
+
+public class TreeScanner
 {
-    public class TreeScanner
+    private readonly Hasher _hasher = new();
+
+    public IEnumerable<IFileItem> ScanTree(string path, int degreeOfParallelism = 1)
     {
-        private readonly Hasher _hasher = new Hasher();
+        var di = new DirectoryInfo(path);
 
-        public IEnumerable<IFileItem> ScanTree(string path, int degreeOfParallelism = 1)
+        var result = di.EnumerateFiles("*", SearchOption.AllDirectories)
+            .Select(x => new FileItem { Path = x.FullName, Size = x.Length })
+            .AsParallel().WithDegreeOfParallelism(degreeOfParallelism)
+            .Select(CalculateHash)
+            .OrderBy(x => x.Path)
+            .ToArray();
+
+        return result;
+    }
+
+    public Task<IEnumerable<IFileItem>> ScanTreeAsync(string path, int degreeOfParallelism = 1)
+    {
+        return Task.Run(() => ScanTree(path, degreeOfParallelism));
+    }
+
+    private FileItem CalculateHash(FileItem item)
+    {
+        using (var stream = new FileStream(item.Path, FileMode.Open, FileAccess.Read, FileShare.Read))
         {
-            var di = new DirectoryInfo(path);
-
-            var result = di.EnumerateFiles("*", SearchOption.AllDirectories)
-                .Select(x => new FileItem() { Path = x.FullName, Size = x.Length })
-                .AsParallel().WithDegreeOfParallelism(degreeOfParallelism)
-                .Select(CalculateHash)
-                .OrderBy(x => x.Path)
-                .ToArray();
-
-            return result;
+            var hash = _hasher.HashStream(stream);
+            item.Hash = hash;
         }
 
-        private FileItem CalculateHash(FileItem item)
-        {
-            using (var stream = new FileStream(item.Path, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                var hash = _hasher.HashStream(stream);
-                item.Hash = hash;
-            }
+        return item;
+    }
 
-            return item;
+    private async Task<FileItem> CalculateHashAsync(FileItem item)
+    {
+        using (var stream = new FileStream(item.Path, FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            var hash = await _hasher.HashStreamAsync(stream);
+            item.Hash = hash;
         }
+
+        return item;
     }
 }
